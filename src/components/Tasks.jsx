bd99151fd8c0b96350b3e7057cc7a12f3e1c0f19
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2026-01-31 00:01:21 -0300

    initial commit

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Header from "./Header";
+
+function Tasks() {
+  const [inputValue, setInputValue] = useState();
+  const [messages, setMessages] = useState(["Task 1", "Task 2", "Task 3"]);
+
+  function handleButtonClick() {
+    setMessages([...messages, inputValue]);
+    setInputValue("");
+  }
+
+  return (
+    <div>
+      <Header>Add a Tasks</Header>
+
+      <input
+        className="input"
+        type="text"
+        placeholder="Create your task..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+
+      <button className="button" onClick={handleButtonClick}>
+        Add Task
+      </button>
+
+      <Header>My Tasks</Header>
+
+      <div>
+        <ul>
+          {messages.map((message) => {
+            return <li key={message}>{message}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Tasks;
