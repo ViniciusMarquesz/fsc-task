0a47cd273387b9d13ae8c8c12c6bc9a554a12b03
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2026-01-31 14:00:18 -0300

    chore: add tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import Tasks from "./components/Tasks";
-
 function App() {
-  return <Tasks />;
+  return (
+    <div>
+      <h1 className="text-red-500 text-4xl font-bold">
+        Tailwind funcionando 🚀
+      </h1>{" "}
+    </div>
+  );
 }
 
 export default App;
