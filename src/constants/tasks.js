a345b90f80a19b1abfe35fab139299f8127bee7d
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2026-01-31 21:41:51 -0300

    feat: ensure tasks are listed on Tasks component

---
+++ src/constants/tasks.js
@@ -0,0 +1,46 @@
+const tasksList = [
+  {
+    id: 1,
+    title: "Estudar FSC",
+    descripition: "Estudar para a prova de FSC",
+    time: "morning",
+    status: "done",
+  },
+  {
+    id: 2,
+    title: "Estudar React",
+    descripition: "Estudar para a prova de React",
+    time: "morning",
+    status: "done",
+  },
+  {
+    id: 3,
+    title: "Estudar TypeScript",
+    descripition: "Estudar para a prova de TypeScript",
+    time: "afternoon",
+    status: "in_progress",
+  },
+  {
+    id: 4,
+    title: "Estudar Next",
+    descripition: "Estudar para a prova de Next",
+    time: "afternoon",
+    status: "in_progress",
+  },
+  {
+    id: 5,
+    title: "Descansar",
+    descripition: "Assistir um filme com a namorada",
+    time: "evening",
+    status: "not_started",
+  },
+  {
+    id: 6,
+    title: "Dormir",
+    descripition: "Dormir porque o homem não é de ferro",
+    time: "evening",
+    status: "not_started",
+  },
+];
+
+export default tasksList;
