5160699667e06ff2423e7e8719732521679ee021
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2026-02-02 16:15:17 -0300

    feat: add  TimeSelect component

---
+++ src/components/TimeSelect.jsx
@@ -0,0 +1,17 @@
+import InputLabel from "./InputLabel";
+
+const TimeSelect = () => {
+  return (
+    <div className="flex flex-col gap-1 text-left">
+      <InputLabel htmlFor="time">Horário</InputLabel>
+
+      <select className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F] focus:border-[#00ADB5]">
+        <option value="morning">Manhã</option>
+        <option value="afternoon">Tarde</option>
+        <option value="evening">Noite</option>
+      </select>
+    </div>
+  );
+};
+
+export default TimeSelect;
