caca4f672078141ad250585cc333fbf412e12adf
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2026-02-02 15:23:50 -0300

    feat: add size and ghost/secondary variant to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,37 @@
-const Button = ({ children, variant = "primary", ...rest }) => {
+const Button = ({
+  children,
+  variant = "primary",
+  size = "small",
+  className,
+  ...rest
+}) => {
   const getVariantClasses = () => {
     if (variant === "primary") {
       return "bg-[#00ADB5] text-white";
     }
 
-    if (variant === "secondary") {
+    if (variant === "ghost") {
       return "bg-transparent text-[#818181]";
+    }
+
+    if (variant === "secondary") {
+      return "bg-[#EEEEEE] text-[#35383E]";
+    }
+  };
+
+  const getSizeClasses = () => {
+    if (size === "small") {
+      return "text-xs  py-1";
+    }
+
+    if (size === "large") {
+      return "text-sm  py-2";
     }
   };
 
   return (
     <button
-      className={`flex items-center gap-2 rounded-md px-3 py-1 font-semibold transition hover:opacity-75 ${getVariantClasses()}`}
+      className={`flex items-center justify-center gap-2 rounded-md px-3 font-semibold transition hover:opacity-75 ${getVariantClasses()} ${getSizeClasses()} ${className}`}
       {...rest}
     >
       {children}
