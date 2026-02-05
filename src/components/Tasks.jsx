559a577e0fce6698839700114115d6fe67daedc0
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2026-02-05 14:10:29 -0300

    feat: ensure tasks is created on db

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -70,7 +70,18 @@ const Tasks = () => {
     setTasks(newTasks);
   };
 
-  const handleAddTaskSubmit = (task) => {
+  const handleAddTaskSubmit = async (task) => {
+    // Chamar API para adicionar tarefa
+    const response = await fetch("http://localhost:3000/tasks", {
+      method: "POST",
+      body: JSON.stringify(task),
+    });
+
+    if (!response.ok) {
+      toast.error("Erro ao adicionar tarefa!");
+      return;
+    }
+
     setTasks([...tasks, task]);
     toast.success("Tarefa adicionada com sucesso!");
   };
