ae33d55af89201130c0581d8dde9bc123fcac96a
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2026-02-22 17:57:03 -0300

    feat: add home and tasks page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../components/sidebar";
+
+const HomePage = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default HomePage;
